45e19c2d5805405c40c20d58ec0eaf46cd8c5feb
MMMMZZKK <zkrny9824@gmail.com>
2024-10-09 23:26:04 +0300

    Update insurance_Model.py

--- insurance_Model.py
+++ insurance_Model.py
@@ -1,34 +1,44 @@
-
-
+import os
 import pandas as pd
+import joblib
 from pycaret.regression import load_model, predict_model
 from fastapi import FastAPI
+#from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel  # تغيير إلى BaseModel لنماذج الإدخال والإخراج
-from fastapi import FastAPI, Response
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-
+from pydantic import BaseModel
 
 # إنشاء التطبيق
 app = FastAPI()
 
+# تحديد مسار الموديل
+model_dir = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(model_dir, '1-Insurance_model')
 
+print(f"Loading model from: {model_path}")
 
-# تحميل نموذج التدريبات
-model = load_model("1-Insurance_model")
+# تحميل الموديل
+try:
+    model = load_model(model_path)
+except Exception as e:
+    print(f"Failed to load model with pycaret: {e}")
+    try:
+        model = joblib.load(model_path)
+        print("Model loaded successfully with joblib.")
+    except Exception as e:
+        print(f"Failed to load model with joblib: {e}")
+        raise e
 
-
+# إعداد CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # يمكنك تحديد نطاقات معينة هنا
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    )
+)
 
 # تعريف نماذج Pydantic للإدخال والإخراج
-class InputModel(BaseModel):  # استخدام BaseModel بدلاً من create_model
+class InputModel(BaseModel):
     age: int
     sex: str
     bmi: float
@@ -36,35 +46,17 @@ class InputModel(BaseModel):  # استخدام BaseModel بدلاً من create_
     smoker: str
     region: str
 
-class OutputModel(BaseModel):  # استخدام BaseModel لنموذج الإخراج
+class OutputModel(BaseModel):
     prediction: float
 
+# الدالة الرئيسية لإرجاع صفحة HTML
+#@app.get("/", response_class=HTMLResponse)
+#def read_root():
+#    return FileResponse("index.html")
 
-
-#app.mount("/static", StaticFiles(directory="static"), name="static")
-
-##@app.get("/", response_class=HTMLResponse)
-#async def read_root():
- #   with open("index.html", "r", encoding="utf-8") as f:
- #       return f.read()
-
-
-
-
-# تعريف دالة التنبؤ
-@app.post("/predict")#, response_model=OutputModel)
-
+# دالة التنبؤ
+@app.post("/predict", response_model=OutputModel)
 def predict(data: InputModel):
-    
-    data_df = pd.DataFrame([data.dict()])  # تحويل الإدخال إلى DataFrame
+    data_df = pd.DataFrame([data.dict()])
     predictions = predict_model(model, data=data_df)
-    return OutputModel(prediction=predictions["prediction_label"].iloc[0])  # إرجاع OutputModel
-
-    
-    
-    
-    
-    
-
-
-
+    return OutputModel(prediction= round(predictions['prediction_label'].iloc[0], 2))
